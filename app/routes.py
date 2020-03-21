bf3d5a0dbd699beffa19acb4a8e502da5e60fb74
tfukaza <tomokifukazawa123@gmail.com>
2020-03-20 22:50:23 -0700

    chnaged req from js to JSON

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,20 @@
-from app import app
+#from app import app
 from flask import request
+from flask import Flask
 import db
+
+app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     print(request.args.get('test'))
-    db.open_connection("../db.config")
-    result = db.execute_db("SELECT * FROM courses JOIN slots USING(course_id);")
+    db.open_connection()
+    result = db.execute_db("SELECT * FROM departments")
     db.close_connection()
 
     return result[0][0]
+
+if __name__ == "__main__":
+    app.debug = True 
+    app.run(host="0.0.0.0", port=80)
